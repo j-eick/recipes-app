41b9a0c7128dd5187ba06328d3c4faae1cdc43b4
Joschka Eickhoff <jeickhoff@posteo.de>
2022-08-02 15:19:27 +0200

    basic setup

--- pages/dessert.js
+++ pages/dessert.js
@@ -1,3 +1,16 @@
+import Image from "next/image";
+
 export default function Dessert() {
-  return <p>Dessert</p>;
+  return (
+    <>
+      <p>Dessert</p>
+      <Image
+        src="https://unsplash.com/photos/surQ2mkZNxw/download?ixid=MnwxMjA3fDB8MXxzZWFyY2h8Mzd8fGRlc3NlcnR8ZW58MHwwfHx8MTY1OTMzNzk1NQ&force=true&w=2400"
+        alt="futter"
+        height="400"
+        width="600"
+        layout="responsive"
+      />
+    </>
+  );
 }
--- pages/veggie.js
+++ pages/veggie.js
@@ -1,3 +1,16 @@
+import Image from "next/image";
+
 export default function Veggie() {
-  return <p>Veggie</p>;
+  return (
+    <>
+      <p>Veggie</p>
+      <Image
+        src="/pizza.jpeg"
+        layout="responsive"
+        width="500"
+        height="400"
+        alt="essen"
+      />
+    </>
+  );
 }
